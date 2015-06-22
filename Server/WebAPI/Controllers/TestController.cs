1136e3258a6eb43153397384d9e469dc9364a099
Kevin Main <kevmain@outlook.com>
2015-06-22 14:57:24 +0100

    Rebuilt solution and recreated as references were failing to work

---
+++ Server/WebAPI/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class TestController : ApiController
+    {
+        [HttpGet]
+        [Route("test")]
+        public HttpResponseMessage GetTestMessage()
+        {
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, "Test Message");
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+    }
+}
