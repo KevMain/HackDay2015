0db019cf9bbdc584fa47dd06d7384de4430532e4
Kevin Main <kevmain@outlook.com>
2015-06-23 09:34:12 +0100

    Missed some files

---
+++ Server/WebAPI/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Web;
+using API;
+using API.Modules;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+using Ninject;
+using Ninject.Web.Common;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
+
+namespace API
+{
+    public static class NinjectWebCommon
+    {
+        private static readonly Bootstrapper Bootstrapper = new Bootstrapper();
+
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            Bootstrapper.Initialize(CreateKernel);
+        }
+
+        public static void Stop()
+        {
+            Bootstrapper.ShutDown();
+        }
+
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+
+                RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Load(new MainModule());
+        }
+    }
+}
---
+++ Server/WebAPI/Modules/MainModule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Ninject.Modules;
+
+namespace API.Modules
+{
+    public class MainModule : NinjectModule
+    {
+        public override void Load()
+        {
+        }
+    }
+}
